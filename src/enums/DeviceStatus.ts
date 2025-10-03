5b149a3839c80ba4b9df2cd28a6de419fbaa3012
Jonathan <many.d.r.o.2001@hotmail.com>
2025-10-03 18:03:00 -0500

    Network Device Interface added

---
+++ src/enums/DeviceStatus.ts
@@ -0,0 +1,10 @@
+/**
+ * Real-time device status
+ */
+export enum DeviceStatus {
+  ONLINE = 'online',
+  OFFLINE = 'offline',
+  DEGRADED = 'degraded',
+  UNKNOWN = 'unknown',
+  MAINTENANCE = 'maintenance'
+}
---
+++ src/enums/DeviceVendor.ts
@@ -0,0 +1,12 @@
+/**
+ * Supported network device vendors
+ */
+export enum DeviceVendor {
+  MIKROTIK = 'mikrotik',
+  UBIQUITI = 'ubiquiti',
+  MIMOSA = 'mimosa',
+  CISCO = 'cisco',
+  ARUBA = 'aruba',
+  CAMBIUM = 'cambium',
+  OTHER = 'other'
+}
