8ca2fe887e619cfa8a7def2b728f1f16919853f0
Jonathan <many.d.r.o.2001@hotmail.com>
2025-12-06 11:22:00 -0500

    Modified the Entity to work with the new implementation of UniqueIdentityID

--- src/domain/shared/kernel/Entity.ts
+++ src/domain/shared/kernel/Entity.ts
@@ -9,13 +9,14 @@ import { UniqueEntityID } from './';
  * comparing their IDs.
  *
  * @template T The shape of the entity's properties.
+ * @template TID The type of the entity's unique identifier, extending {@link UniqueEntityID}.
  */
-export abstract class Entity<T> {
+export abstract class Entity<T, TID extends UniqueEntityID> {
   /**
    * The unique identifier of the entity.
    * @protected
    */
-  protected readonly _id: UniqueEntityID;
+  protected readonly _id: TID;
 
   /**
    * The internal properties of the entity.
@@ -27,19 +28,19 @@ export abstract class Entity<T> {
    * Creates a new entity instance.
    *
    * @param {T} props - The properties that define the entity's state.
-   * @param {UniqueEntityID} [id] - Optional pre-existing unique identifier. If omitted, a new one is generated.
+   * @param {TID} id - The unique identifier for this entity. Must be created by the concrete entity's factory method.
    */
-  constructor(props: T, id?: UniqueEntityID) {
-    this._id = id ? id : new UniqueEntityID();
+  protected constructor(props: T, id: TID) {
+    this._id = id;
     this.props = props;
   }
 
   /**
    * Gets the unique identifier of the entity.
    *
-   * @returns {UniqueEntityID} The entity's ID.
+   * @returns {TID} The entity's ID type.
    */
-  get id(): UniqueEntityID {
+  get id(): TID {
     return this._id;
   }
 
@@ -50,10 +51,10 @@ export abstract class Entity<T> {
    * - They are the same reference, or
    * - They are both entities and their IDs match.
    *
-   * @param {Entity<T>} [object] - The entity to compare with.
+   * @param {Entity<T, TID>} [object] - The entity to compare with.
    * @returns {boolean} True if both represent the same entity, otherwise false.
    */
-  public equals(object?: Entity<T>): boolean {
+  public equals(object?: Entity<T, TID>): boolean {
     if (object === null || object === undefined) {
       return false;
     }
@@ -66,7 +67,7 @@ export abstract class Entity<T> {
       return false;
     }
 
-    return this._id.equals(object._id);
+    return this._id.equals(object.id);
   }
 
   /**
@@ -74,9 +75,9 @@ export abstract class Entity<T> {
    *
    * @private
    * @param {unknown} v - The value to check.
-   * @returns {v is Entity<unknown>} True if the value is an Entity instance.
+   * @returns True if the value is an Entity instance.
    */
-  private static isEntity(v: unknown): v is Entity<unknown> {
+  public static isEntity(v: unknown) {
     return v instanceof Entity;
   }
 }
