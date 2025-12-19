58d89f4a8e59c93a8804015ca176107bbe347bd6
Jonathan <many.d.r.o.2001@hotmail.com>
2025-12-19 16:52:44 -0500

    MACAddress value object changed to follow the value object standard. Tests also updated and props moved to a new file

--- src/domain/value-objects/MACAddress.ts
+++ src/domain/value-objects/MACAddress.ts
@@ -1,4 +1,4 @@
-import { ValueObject, Result, Guard } from '../';
+import { ValueObject, Result, Guard, MACAddressProps } from '../';
 
 /**
  * MACAddress Value Object
@@ -7,15 +7,27 @@ import { ValueObject, Result, Guard } from '../';
  * Supports both colon-separated (AA:BB:CC:DD:EE:FF) and hyphen-separated (AA-BB-CC-DD-EE-FF) formats.
  * Internally normalized to colon-separated uppercase format.
  *
+ * Business Rules:
+ * - MAC address cannot be null, undefined, or empty
+ * - Must be in format XX:XX:XX:XX:XX:XX or XX-XX-XX-XX-XX-XX
+ * - Each segment (XX) must be a valid hexadecimal number (0-9, A-F)
+ * - Must contain exactly 6 segments (48-bit address)
+ * - Automatically normalized to uppercase with colon separators
+ * - Both colon and hyphen separators are accepted on input
+ *
  * @example
  * const macResult = MACAddress.create('AA:BB:CC:DD:EE:FF');
- * const macResult2 = MACAddress.create('aa-bb-cc-dd-ee-ff'); // Also valid, normalized to AA:BB:CC:DD:EE:FF
+ * if (macResult.isSuccess) {
+ *   const mac = macResult.value;
+ *   console.log(mac.toString()); // 'AA:BB:CC:DD:EE:FF'
+ *   console.log(mac.toHyphenFormat()); // 'AA-BB-CC-DD-EE-FF'
+ *   console.log(mac.toCompactFormat()); // 'AABBCCDDEEFF'
+ * }
+ *
+ * @example
+ * const macResult2 = MACAddress.create('aa-bb-cc-dd-ee-ff');
+ * // Also valid, automatically normalized to 'AA:BB:CC:DD:EE:FF'
  */
-
-interface MACAddressProps {
-  value: string; // Normalized format: AA:BB:CC:DD:EE:FF
-}
-
 export class MACAddress extends ValueObject<MACAddressProps> {
   get value(): string {
     return this.props.value;
@@ -27,6 +39,10 @@ export class MACAddress extends ValueObject<MACAddressProps> {
 
   /**
    * Validates if a string is a valid MAC address.
+   *
+   * @param mac - MAC address string to validate
+   * @returns True if valid MAC format, false otherwise
+   *
    * Accepts both colon-separated and hyphen-separated formats.
    */
   public static isValid(mac: string): boolean {
@@ -85,6 +101,8 @@ export class MACAddress extends ValueObject<MACAddressProps> {
 
   /**
    * Returns the normalized MAC address (AA:BB:CC:DD:EE:FF format).
+   *
+   * @returns MAC address in uppercase colon-separated format
    */
   public normalize(): string {
     return this.props.value;
@@ -98,14 +116,18 @@ export class MACAddress extends ValueObject<MACAddressProps> {
   }
 
   /**
-   * Converts the MAC address to hyphen-separated format (AA-BB-CC-DD-EE-FF).
+   * Converts the MAC address to hyphen-separated format.
+   *
+   * @returns MAC address in AA-BB-CC-DD-EE-FF format
    */
   public toHyphenFormat(): string {
     return this.props.value.replace(/:/g, '-');
   }
 
   /**
-   * Returns the MAC address without separators (AABBCCDDEEFF).
+   * Returns the MAC address without separators.
+   *
+   * @returns MAC address in compact format (AABBCCDDEEFF)
    */
   public toCompactFormat(): string {
     return this.props.value.replace(/:/g, '');
